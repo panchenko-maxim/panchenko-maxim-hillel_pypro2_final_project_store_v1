6807104c0728331d093755cc24b2139c4a002163
panchenko-maxim <panchenko.maxim87@google.com>
2025-05-28 00:37:34 +0300

    correct the error via added input and output classes order, add comment in order model

--- shop_backend/store/models.py
+++ shop_backend/store/models.py
@@ -18,9 +18,16 @@ class Product(models.Model):
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    comment = models.TextField(blank=True, null=True)
     products = models.ManyToManyField(Product, through='OrderProduct')
+    
+    def __str__(self):
+        return f"{self.id} - {self.created_at}"
 
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.order.id}) - {self.product.name} - {self.quantity} units"
--- shop_backend/store/serializers.py
+++ shop_backend/store/serializers.py
@@ -17,17 +17,32 @@ class OrderProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderProduct
         fields = '__all__'
+        
+
+class OrderProductInputSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField()
+    quantity = serializers.IntegerField()
+    
+class OrderProductOutputSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['id', 'product', 'quantity', 'order']
 
 class OrderSerializer(serializers.ModelSerializer):
-    products = OrderProductSerializer(source='ordeproduct_set', many=True)
+    products = OrderProductOutputSerializer(source='orderproduct_set', many=True, read_only=True)
+    items = OrderProductInputSerializer(many=True, write_only=True)
 
     class Meta:
         model = Order
-        fields = ['id', 'created_at', 'products']
+        fields = ['id', 'created_at', 'comment', 'products', 'items']
         
     def create(self, validated_data):
         items_data = validated_data.pop('items')
         order = Order.objects.create(**validated_data)
         for item in items_data:
-            OrderProduct.objects.create(order=order, **item)
+            OrderProduct.objects.create(
+                order=order,
+                product_id=item['product_id'],
+                quantity=item['quantity']
+            )
         return order
