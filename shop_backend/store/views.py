959a7948203787cd50bdec07f178aff5fa826574
panchenko-maxim <panchenko.maxim87@google.com>
2025-06-04 18:33:15 +0300

    correct provlem with registration, problem in response tokens

--- shop_backend/store/views.py
+++ shop_backend/store/views.py
@@ -48,12 +48,12 @@ class RegisterView(APIView):
             user = serializer.save()
         
             refresh = RefreshToken.for_user(user)
-            access_token = str(refresh.access_token)
+            access = str(refresh.access_token)
             refresh_token = str(refresh)
-            
+                        
             return Response({
                 'message': 'User registered and logged in successfully',
-                'access_token': access_token,
+                'access_token': access,
                 'refresh_token': refresh_token 
             }, status=status.HTTP_201_CREATED)
         else:
--- shop_frontend/src/views/RegisterView.vue
+++ shop_frontend/src/views/RegisterView.vue
@@ -24,8 +24,8 @@ export default {
         async register() {
             try {
                 const response = await axios.post('http://127.0.0.1:8000/api/auth/register/', this.credentials)
-                localStorage.setItem('access_token', response.data.access)
-                localStorage.setItem('refresh_token', response.data.refresh)
+                localStorage.setItem('access_token', response.data.access_token)
+                localStorage.setItem('refresh_token', response.data.refresh_token)
                 localStorage.setItem('username', this.credentials.username)
                 this.$router.push('/')
             } catch (error) {
